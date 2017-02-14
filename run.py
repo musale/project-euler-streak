83f43d3d6c3a10f823e83f30580d435c51d66f14
musale <martinmshale@gmail.com>
2017-02-14 19:21:20 +0300

    :wink: shifted to working with the github api

--- run.py
+++ run.py
@@ -1,84 +1,155 @@
 #!/usr/bin/env python
 """Run file to execute the eauler streak maker."""
+import json
 import os
 from os.path import dirname, join
 
 import dotenv
 import requests
 
-MAX_CHARS = 140
-
 # load the environment variables
 dotenv_path = join(dirname("__file__"), '.env')
 dotenv.read_dotenv(dotenv_path)
 GITHUB_USERNAME = os.environ.get("GITHUB_USERNAME")
-GITHUB_PASS = os.environ.get("GITHUB_PASS")
-GITHUB_URL = "https://+%s+:+%s+@github.com/+%s+/project-euler-streak.git" % (
-    GITHUB_USERNAME, GITHUB_PASS, GITHUB_USERNAME,)
+GITHUB_REPO = os.environ.get("GITHUB_REPO")
+
+counter_value = open("counter.txt", "r").readline()
+
+# POST /repos/:owner/:repo/git/commits
+COMMIT_URL = "https://api.github.com/repos/{0}/{1}/git/commits".format(
+    GITHUB_USERNAME, GITHUB_REPO)
+
+MASTER = "https://api.github.com/repos/{0}/{1}/git/refs/heads/master".format(
+    GITHUB_USERNAME, GITHUB_REPO)
+TREES = "https://api.github.com/repos/{0}/{1}/git/trees".format(
+    GITHUB_USERNAME, GITHUB_REPO)
 
 
-# 1. open the counter file and check current file counter
-counter = open("counter.txt", "r")
-counter_value = counter.readline()
-counter_value = counter_value[:-1]
+def get_head():
+    """Get the sha of the master branch."""
+    response = requests.get(MASTER)
+    last_commit_sha = json.loads(response.content)['object']['sha']
+    print last_commit_sha
+    return last_commit_sha
+
+
+def get_last_commit(LAST_COMMIT_URL):
+    """Get the last commit."""
+    response = requests.get(LAST_COMMIT_URL)
+    last_tree_sha = json.loads(response.content)['tree']['sha']
+    return last_tree_sha
+
+
+def create_tree_obj(base_tree, path):
+    """Create tree object (also implicitly creates a blob based on content)."""
+    payload = {
+        "base_tree": base_tree,
+        "tree": [
+            {
+                "path": path,
+                "mode": "100644",
+                "type": "blob",
+            }
+        ]
+    }
+    response = requests.post(TREES, json=payload)
+    new_content_tree_sha = json.loads(response.content)['sha']
+    return new_content_tree_sha
+
+
+def create_commit(last_commit_sha, new_content_tree_sha, message):
+    """Create a new commit and POSTS to the COMMIT_URL."""
+    payload = {
+        "parents": [last_commit_sha],
+        "tree": new_content_tree_sha,
+        "message": message
+    }
+    print payload
+    response = requests.post(COMMIT_URL, json=payload)
+    print json.loads(response.content)
+    new_commit_sha = json.loads(response.content)['sha']
+    return new_commit_sha
+
+
+def update_branch(new_commit_sha):
+    """Update the branch with new commit."""
+    payload = {
+        "ref": "refs/heads/master",
+        "sha": new_commit_sha
+    }
+    response = requests.patch(COMMIT_URL, json=payload)
+    return response.status
 
 
 def main():
     """Starting point of execution."""
+    # get the head of the master branch
+    last_commit_sha = get_head()
+    LAST_COMMIT = "https://api.github.com/repos/{0}/{1}/git/commits/{2}".format(
+        GITHUB_USERNAME, GITHUB_REPO, last_commit_sha)
+    # get the last commit
+    last_tree_sha = get_last_commit(LAST_COMMIT)
+
     if len(counter_value) == 0:
         # nothing has been written to counter.
         # will probably run just once
         # write to it 001
-        commit_message = create_counter_commit("001")
-
-        # now run the first euler problem into 001.py
-        # it always asks for input in order to create file so
-        # do it automatically by starting with yes and piping euler command
-        os.system("yes|euler")
-
-        # add the problem py file
-        os.system("git add .")
-
-        # commit the problem py file
-        os.system("git commit -m '{0}'".format(commit_message,))
-
-        # push it all upstream
-        os.system("git push origin master")
+        create_counter_commit("001", last_commit_sha, last_tree_sha)
     else:
-        commit_message = create_counter_commit(counter_value)
-        os.system("yes|euler {0}".format(counter_value))
-
-        # add the problem py file
-        os.system("git add .")
-
-        # commit the problem py file
-        os.system("git commit -m '{0}'".format(commit_message,))
-
-        # push it all upstream
-        os.system("git push origin master")
+        create_counter_commit(counter_value, last_commit_sha, last_tree_sha)
 
 
-def create_counter_commit(count):
+def create_counter_commit(count, last_commit_sha, last_tree_sha):
     """Create a counter write and commit message."""
-    printer("writing counter file ...")
     counter = open("counter.txt", "w")
     counter.write(count)
     counter.close()
+    commit_message = "Set up new problem {0}".format(count,)
+    new_content_tree_sha = create_tree_obj(last_tree_sha, "counter.txt")
 
-    # add the counter.txt file
-    os.system("yes|git add -p counter.txt")
+    new_commit_sha = create_commit(
+        last_commit_sha, new_content_tree_sha, commit_message)
+    update_branch(new_commit_sha)
 
+    # now run the first euler problem into 001.py
+    # it always asks for input in order to create file so
+    # do it automatically by starting with yes and piping euler command
+    if count.count('0') == 1:
+        new_count = "00{0}".format(str(int(count) + 1))
+    if count.count('0') >= 2:
+        new_count = "0{0}".format(str(int(count) + 1))
+
+    # get new values
+    last_commit_sha = get_head()
+    LAST_COMMIT = "https://api.github.com/repos/{0}/{1}/commits/#{2}".format(
+        GITHUB_USERNAME, GITHUB_REPO, last_commit_sha)
+    # get the last commit
+    last_tree_sha = get_last_commit(LAST_COMMIT)
+    print "LAST COMMIT"
+    print last_tree_sha
+    if count == '001':
+        os.system("yes|euler")
+        create_problem_file_commit(new_count, last_commit_sha, last_tree_sha)
+    else:
+        os.system("yes|euler {0}".format(new_count))
+        create_problem_file_commit(new_count, last_commit_sha, last_tree_sha)
+
+
+def create_problem_file_commit(count, last_commit_sha, last_tree_sha):
+    """Create a commit for the problem file."""
     # create commit for new problem file
     commit_message = "Euler Problem {0} file added\n - This is the first commit for the number {1} problem".format(
         count, count)
+    new_content_tree_sha = create_tree_obj(
+        last_tree_sha, "{0}.py".format(count))
 
-    # commit the counter.txt file
-    os.system("git commit -m 'Set up problem {0}'".format(count,))
-    return commit_message
+    new_commit_sha = create_commit(
+        last_commit_sha, new_content_tree_sha, commit_message)
+    status = update_branch(new_commit_sha)
+    print status
+    print "DONE"
+    return status
 
-
-def printer(message):
-    print message
 
 if __name__ == "__main__":
     main()
